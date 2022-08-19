fed17aea1e02019e1be5b849f681e99f4fb15ba2
Stefan Heid <stefan.heid@upb.de>
2022-08-19 13:01:28 +0200

    add Evaluator class that wraps the exec and eval command

--- src/test/kotlin/scratch/Jython.kt
+++ src/test/kotlin/scratch/Jython.kt
@@ -3,25 +3,46 @@ package scratch
 
 import org.python.core.PyBoolean
 import org.python.core.PyDictionary
+import org.python.core.PyInteger
+import org.python.core.PyString
+import org.python.modules._collections.PyDefaultDict
 import org.python.util.PythonInterpreter
 
 
 fun main() {
     // initialization
-    val interp = PythonInterpreter()
-    interp.exec("from collections import defaultdict")
-    interp.exec("loc = defaultdict(int)")
+    val evaluator = Evaluator()
 
-    // evaluating expressions
-    interp.exec("res = eval(\"x<4\",globals(),loc)")
-    println((interp["res"] as PyBoolean).booleanValue)
-
-    // executing statements
-    interp.exec("exec (\"y+=1\",globals(),loc)")
-    val vars = interp["loc"]
-    println((vars as PyDictionary).toKt())
+    println(evaluator.eval("x<5", mapOf("x" to 10)))
+    println(evaluator.exec("y+=1"))
 }
 
-private fun PyDictionary.toKt(): Map<String, Int> {
-    return this.toList().associate { it.first as String to it.second as Int }
+
+class Evaluator {
+    private val interp = PythonInterpreter().apply {
+        exec("from collections import defaultdict")
+    }
+
+    private fun Map<String, Int>.toDefaultDict(): PyDefaultDict {
+        return PyDefaultDict(
+            null,
+            this.entries.associate { PyString(it.key) to PyInteger(it.value) }).apply {
+            defaultFactory = PyInteger.TYPE
+        }
+    }
+
+    fun eval(expr: String, vars: Map<String, Int> = mapOf()): Boolean {
+        // evaluating expressions
+        interp.set("loc", vars.toDefaultDict())
+        interp.exec("res = eval(\"${expr}\",{},loc)")
+        return (interp["res"] as PyBoolean).booleanValue
+    }
+
+    fun exec(stmt: String, vars: Map<String, Int> = mapOf()): Map<String, Int> {
+        // executing statements
+        interp.set("loc", vars.toDefaultDict())
+        interp.exec("exec (\"${stmt}\",{},loc)")
+        val outVars = interp["loc"]
+        return (outVars as PyDictionary).toList().associate { it.first as String to it.second as Int }
+    }
 }
