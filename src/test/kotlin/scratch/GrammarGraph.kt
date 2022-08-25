4c6292010c223feaf89ff0993401607834454f87
ajrox <ajrocks0509@gmail.com>
2022-08-25 15:09:06 +0200

    updated datatypes for Nodes to be final

--- src/test/kotlin/scratch/GrammarGraph.kt
+++ src/test/kotlin/scratch/GrammarGraph.kt
@@ -4,6 +4,7 @@ import isml.aidev.Grammar
 import isml.aidev.RuleEdge
 import isml.aidev.Symbol
 import isml.aidev.Symbol.*
+import org.antlr.v4.runtime.misc.OrderedHashSet
 import org.jgrapht.Graphs
 import org.jgrapht.graph.DefaultDirectedGraph
 import org.jgrapht.graph.DefaultEdge
@@ -12,14 +13,23 @@ import org.jgrapht.nio.DefaultAttribute
 import org.jgrapht.nio.dot.DOTExporter
 import java.io.File
 
-open class Node(open var nodes: List<Symbol>) {
+open class Node(open var nodes: LinkedHashSet<Symbol>) {
     val value: String
         get() = nodes.toString()
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Node
+        return hashCode() == other.hashCode()
+    }
 }
-data class ComplexNode(override var nodes: List<Symbol>) : Node(nodes)
-open class SimpleNode(override var nodes: List<Symbol>) : Node(nodes)
-data class NtNode(override var nodes: List<Symbol>) : SimpleNode(nodes)
-data class TermNode(override var nodes: List<Symbol>) : SimpleNode(nodes)
+
+data class ComplexNode(override var nodes: LinkedHashSet<Symbol>) : Node(nodes)
+open class SimpleNode(override var nodes: LinkedHashSet<Symbol>) : Node(nodes)
 
 class ComplexEdge : DefaultEdge()
 
@@ -52,101 +62,92 @@ fun Grammar.toGraph(): DefaultDirectedGraph<Node, DefaultEdge> {
 
     // Example JGraphT code https://jgrapht.org/guide/UserOverview
     val graph = DefaultDirectedGraph<Node, DefaultEdge>(DefaultEdge::class.java)
+    val nodes = mutableMapOf<String, Node>()
+    val complexNodes = mutableMapOf<String, Node>()
 
-    // TODO convert grammar into a graph
+    fun Node.addToGraph(keyNode: Node): Node {
+        graph.addVertex(this)
+        graph.addEdge(keyNode,this)
+        return this
+    }
+
     this.prodRules.forEach { (key, vallue) ->
-        val keyNode = NtNode(mutableListOf(NonTerminal(key)))
-        graph.addVertex(keyNode)
-
-
-        vallue.forEach { it ->
-            val value = it.substitution
-            if (value.size == 1) {
-                var tempVertex = Node(mutableListOf())
-                if (value[0] is NonTerminal) {
-                    tempVertex = NtNode(mutableListOf(NonTerminal(value[0].value)))
-                } else if (value[0] is Terminal) {
-                    tempVertex = TermNode(mutableListOf(Terminal(value[0].value)))
-                }
-                if (tempVertex !in graph.vertexSet()) {
-                    graph.addVertex(tempVertex)
-                }
-                graph.addEdge(keyNode, tempVertex)
-            } else {
-                var complexString = ""
-                val cs_list: MutableList<Symbol> = mutableListOf()
-                value.forEach { its ->
-                    complexString += " " + its.value
-                    if (its is NonTerminal) {
-                        cs_list.add(NonTerminal(its.value))
-                    } else if (its is Terminal) {
-                        cs_list.add(Terminal(its.value))
-                    } // get rid of it
-                }
-                val cn = ComplexNode(cs_list)
-                if(cn !in graph.vertexSet()) {
-                    graph.addVertex(cn)
-                }
-                graph.addEdge(keyNode, cn)
-////                cs_list.forEach{
-////                    var tempnode = Node(mutableListOf())
-////                    if (it is Terminal){
-////                        tempnode = TermNode(mutableListOf(it))
-////                    }else{tempnode = NtNode(mutableListOf(it))}
-////                    if (tempnode !in graph.vertexSet()) {
-////                        graph.addVertex(tempnode)
-////                    }
-////                    graph.addEdge(cn, tempnode, ComplexEdge())
-//                }
+        val nt = NonTerminal(key)
+        val keyNode = nodes.getOrPut(nt.toString()) {
+            SimpleNode(linkedSetOf(nt)).apply {
+                graph.addVertex(this)
             }
+        }
+
+        vallue.forEach {
+            val value = it.substitution
+            when (value.size) {
+                1 -> {
+                    val ntVal = value.single().toString()
+                    nodes.getOrPut(ntVal) {
+                        SimpleNode(LinkedHashSet(value)).addToGraph(keyNode)
+                    }
+                }
+                else -> {
+                    complexNodes.getOrPut(value.toString()) {
+                        ComplexNode(LinkedHashSet(value)).addToGraph(keyNode)
+                    }
+                }
+            }
+        }
+    }
+
+    complexNodes.values.forEach { c ->
+        c.nodes.filter { it !is Terminal }.forEach {
+            graph.addEdge(c, nodes[it.toString()]!!, ComplexEdge())
         }
     }
     return graph
 }
 
+
 private fun Grammar.Companion.fromGraph(graph: DefaultDirectedGraph<Node, DefaultEdge>): Grammar {
     val startsymbol = NonTerminal(graph.vertexSet().toList()[0].value)
     val grammar = mutableMapOf<String, MutableList<RuleEdge>>()
 
-    graph.vertexSet()
-        .filter{it !is ComplexNode}
-        .forEach{
-        val succs = graph.succs(it)
-        succs.forEach { succ ->
-            val symbols = mutableListOf<Symbol>()
-            if (succ is NtNode) {
-                symbols.add(NonTerminal(succ.value))
-            } else if (succ is TermNode) {
-                symbols.add(Terminal(succ.value))
-            } else if (succ is ComplexNode){
-                // todo: remove trim here, just for testing
-                succ.value.trim().split(" ").forEach{
-                    it_sn ->
-                    val charval = it_sn.toCharArray().single()
-                    if(charval.isLowerCase()){
-                        symbols.add(Terminal(it_sn))
-                    }else{
-                        symbols.add(NonTerminal(it_sn))
-                    }
-
-                }
-//                succ.nodes.forEach{
-//                        it_sn ->
-//                    if (it_sn is NtNode) {
-//                        symbols.add(NonTerminal(it_sn.value))
-//                    } else if (it_sn is TermNode) {
-//                        symbols.add(Terminal(it_sn.value))
+//    graph.vertexSet()
+//        .filter { it !is ComplexNode }
+//        .forEach {
+//            val succs = graph.succs(it)
+//            succs.forEach { succ ->
+//                val symbols = mutableListOf<Symbol>()
+//                if (succ is NtNode) {
+//                    symbols.add(NonTerminal(succ.value))
+//                } else if (succ is TermNode) {
+//                    symbols.add(Terminal(succ.value))
+//                } else if (succ is ComplexNode) {
+//                    // todo: remove trim here, just for testing
+//                    succ.value.trim().split(" ").forEach { it_sn ->
+//                        val charval = it_sn.toCharArray().single()
+//                        if (charval.isLowerCase()) {
+//                            symbols.add(Terminal(it_sn))
+//                        } else {
+//                            symbols.add(NonTerminal(it_sn))
+//                        }
+//
 //                    }
+////                succ.nodes.forEach{
+////                        it_sn ->
+////                    if (it_sn is NtNode) {
+////                        symbols.add(NonTerminal(it_sn.value))
+////                    } else if (it_sn is TermNode) {
+////                        symbols.add(Terminal(it_sn.value))
+////                    }
+////                }
 //                }
-            }
-            val redge = RuleEdge(substitution = symbols, weight = 1.0F)
-            if (it.value in grammar.keys) {
-                grammar[it.value]?.add(redge)
-            } else {
-                grammar[it.value] = mutableListOf(redge)
-            }
-        }
-    }
+//                val redge = RuleEdge(substitution = symbols, weight = 1.0F)
+//                if (it.value in grammar.keys) {
+//                    grammar[it.value]?.add(redge)
+//                } else {
+//                    grammar[it.value] = mutableListOf(redge)
+//                }
+//            }
+//        }
 
     return Grammar(startsymbol, grammar)
 }
@@ -159,7 +160,7 @@ fun <V, E> DefaultDirectedGraph<V, E>.succs(vert: V): MutableList<V> {
     return Graphs.successorListOf(this, vert)!!
 }
 
-fun <Node, DefaultEdge> DefaultDirectedGraph<Node, DefaultEdge>.simplify(): DefaultDirectedGraph<Node, DefaultEdge> {
+private fun <Node, DefaultEdge> DefaultDirectedGraph<Node, DefaultEdge>.simplify(): DefaultDirectedGraph<Node, DefaultEdge> {
     vertexSet().toList().forEach { node ->
         // The first condition below is important because on deletion of certain nodes during
         // complex nodes' simplification, it throws an error.
@@ -176,21 +177,29 @@ fun <Node, DefaultEdge> DefaultDirectedGraph<Node, DefaultEdge>.simplify(): Defa
 
 
                     } else if (getEdge(pred, node)!!.javaClass == ComplexEdge().javaClass
+                        && pred is ComplexNode
+                        && node is SimpleNode
                         && succ is SimpleNode
-                    ){
+                    ) {
                         // complex node -> NT -> Terminal
                         // pred         -> node -> succ
                         // predecessor is a complex node
                         // modify pred to point to succ
-//                        addEdge(pred, succ, ComplexEdge() as DefaultEdge)
-//                        // modify the internal value of the predecessor according to the succ
-//                        (pred as ComplexNode).value = (pred as ComplexNode).value.replace((node as SimpleNode).value, (succ as SimpleNode).value)
-//                        removeVertex(node)
-//                        // todo: After above simplification, check if complex node points to only terminals and then remove them.
-//                        if(succ is TermNode)
-//                            removeVertex(succ)
-//                        // update ComplexNode.nodes
-//                        println("")
+                        addEdge(pred, succ, ComplexEdge() as DefaultEdge)
+                        // modify the internal value of the predecessor according to the succ
+
+                        removeVertex(node)
+                        // todo: After above simplification, check if complex node points to only terminals and then remove them.
+                        if(succ.nodes.all {  it is Terminal} ){
+                            // remove this succ
+                            removeVertex(succ)
+                            // and update complexnode
+                          //  pred.nodes[node.nodes.single()]
+                        }
+
+                        // update ComplexNode.nodes
+
+                        println("")
                     }
                 }
         }
--- src/test/kotlin/scratch/GrammarGraphTest.kt
+++ src/test/kotlin/scratch/GrammarGraphTest.kt
@@ -9,14 +9,14 @@ class GrammarGraphTest {
     @Test
     fun nodeTest() {
         val grammar = Grammar.fromResource("extremely_simple_gram.yml")
-        val node1 = Node(mutableListOf(grammar.startSymbol))
+        val node1 = Node(linkedSetOf(grammar.startSymbol))
         println(node1.value)
     }
 
     @Test
     fun toGraphTest(){
         val grammar = Grammar.fromResource("extremely_simple_gram.yml")
-        var graph = grammar.toGraph()
-        graph = graph.simplify()
+        val graph = grammar.toGraph()
+        assert(graph.vertexSet().toList().size == 10)
     }
 }
